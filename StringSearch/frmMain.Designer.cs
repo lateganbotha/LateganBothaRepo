e4c73d27610102fbf8d255b576d704b809e02a0d
lateganbotha <lateganbotha@gmail.com>
2017-11-27 18:02:12 +0200

    String Search
    String Search

---
+++ StringSearch/frmMain.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace StringSearch
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtSearchString = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstResults = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(115, 13);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(240, 26);
+            this.txtPath.TabIndex = 50;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(362, 12);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(122, 37);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(362, 57);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(122, 37);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtSearchString
+            // 
+            this.txtSearchString.Location = new System.Drawing.Point(115, 59);
+            this.txtSearchString.MaxLength = 5;
+            this.txtSearchString.Name = "txtSearchString";
+            this.txtSearchString.Size = new System.Drawing.Size(240, 26);
+            this.txtSearchString.TabIndex = 0;
+            this.txtSearchString.TextChanged += new System.EventHandler(this.txtSearchString_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Search For :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "File";
+            // 
+            // lstResults
+            // 
+            this.lstResults.FormattingEnabled = true;
+            this.lstResults.ItemHeight = 20;
+            this.lstResults.Location = new System.Drawing.Point(17, 132);
+            this.lstResults.Name = "lstResults";
+            this.lstResults.Size = new System.Drawing.Size(467, 244);
+            this.lstResults.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 20);
+            this.label3.TabIndex = 51;
+            this.label3.Text = "Matches:";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 396);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lstResults);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSearchString);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtPath);
+            this.Name = "frmMain";
+            this.Text = "Search";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSearchString;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstResults;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ StringSearch/frmMain.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StringSearch
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void txtSearchString_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                var textboxSender = (TextBox)sender;
+
+                var cursorPosition = textboxSender.SelectionStart;
+
+                textboxSender.Text = Regex.Replace(textboxSender.Text, "[^0-9a-zA-Z]", "");
+
+                textboxSender.SelectionStart = cursorPosition;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dlg = new OpenFileDialog();
+
+                dlg.CheckFileExists = true;
+
+                dlg.CheckPathExists = true;
+
+                dlg.Multiselect = false;
+
+                dlg.Filter = "Text|*.txt|All|*.*";
+
+                dlg.ShowDialog();
+
+                txtPath.Text = dlg.FileName;
+                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lstResults.Items.Clear();
+
+                if (txtPath.Text == string.Empty)
+                {
+                    MessageBox.Show("Please Select a File First");
+
+                    return;
+                }
+
+                if (txtSearchString.Text == string.Empty)
+                {
+                    MessageBox.Show("Please Enter String to Seach.");
+
+                    return;
+                }
+
+                string sorted = string.Empty;
+
+                string inPutString = txtSearchString.Text;
+
+                sorted = SortString(inPutString.ToUpper());
+
+                string line = string.Empty;
+
+                System.IO.StreamReader file = new System.IO.StreamReader(txtPath.Text);
+
+                while ((line = file.ReadLine()) != null)
+                {
+                    string fileString = SortString(line.ToUpper());
+
+                    if (sorted.Equals(fileString))
+                    {
+                        lstResults.Items.Add(line);
+                    }
+                }
+
+                if (lstResults.Items.Count <= 0)
+                {
+                    MessageBox.Show("No matches found!");
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public static string SortString(string input)
+        {
+            try
+            {
+                string returnString = string.Empty;
+
+                string letters = new String(input.Where(Char.IsLetter).OrderBy(c => c).ToArray());
+                string numbers = new String(input.Where(Char.IsNumber).OrderBy(c => c).ToArray());
+
+                return returnString = letters + numbers;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        
+    }
+}
